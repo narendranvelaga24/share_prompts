75d042a06c3ab9e5da93b1c51526e3b073632240
Narendran <175111990+Twenty625@users.noreply.github.com>
2025-06-06 14:04:17 +0530

    Create Prompt Page

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import Form from '@components/Form'
+
+const CreatePrompt = () => {
+  return (
+    <div>CreatePrompt</div>
+  )
+}
+
+export default CreatePrompt
